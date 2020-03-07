bb5f8226fbd17ac2528b24793ab5e89a1e438343
Supreet Singh <supreetsingh106@gmail.com>
2020-03-08 00:53:33 +0530

    initializing project

---
+++ src/main/java/com/xebia/fs101/zohoreplica/ZohoReplicaApplication.java
@@ -0,0 +1,13 @@
+package com.xebia.fs101.zohoreplica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZohoReplicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZohoReplicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xebia/fs101/zohoreplica/ZohoReplicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xebia.fs101.zohoreplica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZohoReplicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
