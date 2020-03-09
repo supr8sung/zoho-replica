aca7186f14308f59afaa0e00d7099906315c8abb
Supreet Singh <supreetsingh106@gmail.com>
2020-03-10 00:15:12 +0530

    issue#2 fixe with some changes

--- src/main/java/com/xebia/fs101/zohoreplica/repository/AttendanceRepository.java
+++ src/main/java/com/xebia/fs101/zohoreplica/repository/AttendanceRepository.java
@@ -4,7 +4,5 @@ import com.xebia.fs101.zohoreplica.entity.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
-
-
+public interface AttendanceRepository extends JpaRepository<Attendance,Long> {
 }
