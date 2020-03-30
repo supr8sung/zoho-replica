dd6433cebb8e2a3cfb8f6a789d2504c34b12ace7
Supreet Singh <supreetsingh106@gmail.com>
2020-03-30 17:40:25 +0530

    code refractor

--- src/main/java/com/xebia/fs101/zohoreplica/config/WebMvcConfig.java
+++ src/main/java/com/xebia/fs101/zohoreplica/config/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package com.xebia.fs101.zohoreplica.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,28 +13,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     }
 
 
-
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        //registry.addViewController("/login")
-      //  /Users/codeMars/Downloads/GIT/Repositories/zoho-replica/front-end/build/index.html
-registry.addViewController("/login").setViewName("login");
-//        registry.addViewController("/home").setViewName("home");
+
+        registry.addViewController("/login").setViewName("login");
+
     }
 
- //   @Override
-//    public void addResourceHandlers(
-//            ResourceHandlerRegistry registry) {
-////
-////        registry.addResourceHandler("/static/**")
-////                .addResourceLocations("/front-end/build/static/");
-////        registry.addResourceHandler("/*.js")
-////                .addResourceLocations("/WEB-INF/view/react/build/");
-////        registry.addResourceHandler("/*.json")
-////                .addResourceLocations("/WEB-INF/view/react/build/");
-////        registry.addResourceHandler("/*.ico")
-////                .addResourceLocations("/WEB-INF/view/react/build/");
-//        registry.addResourceHandler("/index.html")
-//                .addResourceLocations("/front-end/build/index.html");
-//    }
 }
