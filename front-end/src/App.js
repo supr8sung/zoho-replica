1c534efb2284edacbfcb02121cb7d32f0cf0c3ac
Kishore8 <sai.kishore2015@gmail.com>
2020-03-31 21:33:15 +0530

    fixed CircleCI UI warnings

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header  from './components/header';
-import  LoginComponent   from './components/login';
 import  DashBoard   from './components/dashboard';
 import  PageNotFound   from './components/PageNotFound';
-import 'react-toastify/dist/ReactToastify.css';
-import SignUp from './components/signup';
 
 const App = () =>{
-    const notify = () => toast("Wow so easy !");
     return(
         <div>
             <Header/>
             
             <Switch>
                 <Route exact path="/" component={DashBoard}/>
-                <Route exact path="/signup" component={SignUp}/>
                 <Route component={PageNotFound}/>
             </Switch>
         </div>
