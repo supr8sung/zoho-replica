0c8ffe6f0299358dd1366c10e1df19f1fe075a00
Supreet Singh <supreetsingh106@gmail.com>
2020-04-01 06:39:19 +0530

    #issue18 completed

--- src/main/java/com/xebia/fs101/zohoreplica/api/request/ChangePasswordRequest.java
+++ src/main/java/com/xebia/fs101/zohoreplica/api/request/ChangePasswordRequest.java
@@ -2,11 +2,12 @@ package com.xebia.fs101.zohoreplica.api.request;
 
 import javax.validation.constraints.NotBlank;
 public class ChangePasswordRequest {
+    @NotBlank
     private String oldPassword;
     @NotBlank
     private String newPassword;
 
-    public ChangePasswordRequest( String oldPassword, @NotBlank String newPassword) {
+    public ChangePasswordRequest( String oldPassword, String newPassword) {
         this.oldPassword = oldPassword;
         this.newPassword = newPassword;
     }
