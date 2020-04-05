15d6bc42de263ca2b1510856e8f50556558c7773
Kishore8 <sai.kishore2015@gmail.com>
2020-04-05 23:19:00 +0530

    implementeed autocomplete from UI

--- front-end/src/components/header.js
+++ front-end/src/components/header.js
@@ -9,6 +9,41 @@ class Header extends React.Component{
     // componentDidMount() {
     //     const user = this.context;
     //   }
+    constructor(props){
+        super(props);
+        this.users = [
+            {userid: 2,username:'kishore'},
+            {userid: 3,username:'supreet'},
+            {userid: 4,username: 'sai Kishore'},
+            {userid: 5,username: 'messi'},
+            {userid: 6,username: 'Xabi'},
+            {userid: 7,username: 'Mbappe'},
+            {userid: 8,username: 'Ronaldo'},
+            {userid: 9,username: 'Company'},
+            {userid: 10,username: 'Neymar'},
+            {userid: 11,username: 'suprit'},
+        ];
+        this.filteredOptions = [];
+        this.showAutoCompleteOptions = false;
+    }
+    
+
+    searchUsers(e){
+        if(e.target.value && e.target.value.length > 1){
+            this.showAutoCompleteOptions = true;
+            let searchValue = e.target.value;
+            this.filteredOptions = this.users.filter(
+                (option) => option.username.toLowerCase().indexOf(searchValue.toLowerCase()) > -1
+            ); 
+            
+            
+        } else{
+            this.showAutoCompleteOptions = false;
+            this.filteredOptions = [];
+
+        }
+        this.setState([...this.filteredOptions]);
+    }
     handleLogout (){
         fetch.get({
             url: '/logout',
@@ -50,8 +85,13 @@ class Header extends React.Component{
                     
                   <div id="navbarMenuHeroA" className="navbar-menu">
                   <div className="navbar-end searchParent">
-                            <input className="input" type="text" placeholder="Search Employee" />
+                            <input className="input" type="text" onChange={(e) => {this.searchUsers(e)}} placeholder="Search Employee" />
                             <i className="fa fa-search searchEmployee" aria-hidden="true"></i>
+                              {
+                                this.showAutoCompleteOptions ?  <ul className="autoSearch">{this.filteredOptions.map((item) =>
+                                    <li key={item.userid}><span className="userId">{item.userid }&nbsp;-&nbsp;</span>{item.username}</li>
+                                 )}</ul> : null
+                              }  
                         </div>
                       <div className="navbar-end">
                           <Link  className="navbar-item" to="/"><i className="fas fa-home"></i></Link>
